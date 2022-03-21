667d62d0305f567e7dfd3d4e0834375c2ab85e1d
CompEng0001 <sb1501@canterbury.ac.uk>
2022-03-21 13:14:32 +0000

    <init> basic install script for each system, should be universal awaiting testing

---
+++ install.sh
@@ -0,0 +1,78 @@
+#! /usr/bin/env bash
+
+#Installer script for dotfiles etc
+
+# /home/user/pantheon
+DIR="$(cd "$(dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd)"
+STOWCONFIGPATH="~/.config"
+MACHINE=$1
+
+softlinks(){
+	# create soft links from "dotfiles"
+	cd ${MACHINE}
+
+	#config
+	echo "stowing directories and/or files"
+	for d in $PWD/*
+	do
+	   #echo -e "${d}"
+	   BNAME=$(basename ${d})
+      	   echo -e "${BNAME}"
+	   if [[ ${BNAME} == "bash" ]];then
+         	echo "removing .bash*"
+		rm ~/.bash*
+		echo "stowing:\n stow -Svt ~ bash/"
+		stoweth -Svt "~" ${BNAME}
+	   elif [[ ${BNAME} == "Wallpaper" ]];then
+		dircheck "~/Pictures" "${BASENAME}"
+		stoweth "~/Pictures/${BNAME}" "${BNAME}"
+           elif [[ ${BNAME} == "nixos" ]];then
+		dircheck "/etc" ${BNAME}
+                stoweth "/etc/nixos" "${BNAME}"
+	   else
+		dircheck ${STOWCONFIGPATH} $BNAME
+		stoweth "${STOWCONFIGPATH}/" ${BNAME}
+	   fi
+	done
+}
+
+stoweth(){
+	stow -Svt "$1" "$2"
+}
+
+dircheck()
+{
+    if [[ ! -d "$1/$2" ]];
+          echo creating "$1/$2"
+          mkdir "$1/$2"
+    else
+	  echo "$1/$2  exists, making back up then removing original"
+          cp $1/$2 $1/$2.bak
+	  rm  $1/$2/*
+    fi
+}
+
+machinecheck(){
+    case "${MACHINE}" in
+	"nav")
+	softlinks ${MACHINE}
+	;;
+	"nisha")
+	softlinks ${MACHINE}
+	;;
+	"nott")
+	softlinks ${MACHINE}
+	;;
+	"nox")
+	softlinks ${MACHINE}
+	;;
+	"nut")
+	softlinks ${MACHINE}
+	;;
+	"nyx")
+	softlinks ${MACHINE}
+	;;
+    esac
+}
+
+machinecheck ${MACHINE}
