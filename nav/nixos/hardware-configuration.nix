6a79f8283ed1c66284a2ffa5dcac655018076ce4
CompEng0001 <sb1501@canterbury.ac.uk>
2022-05-01 15:51:16 +0100

    <mod> missing the new configuration

--- nav/nixos/hardware-configuration.nix
+++ nav/nixos/hardware-configuration.nix
@@ -4,19 +4,49 @@
 { config, lib, pkgs, modulesPath, ... }:
 
 {
-  imports = [ ];
+  imports =
+    [ 
+	(modulesPath + "/installer/scan/not-detected.nix")
+    ];
 
-  boot.initrd.availableKernelModules = [ "ata_piix" "ohci_pci" "ehci_pci" "sd_mod" "sr_mod" ];
+  boot.initrd.availableKernelModules = [ "xhci_pci" "vmd" "nvme" "usb_storage" "sd_mod" "rtsx_pci_sdmmc" ];
   boot.initrd.kernelModules = [ ];
-  boot.kernelModules = [ ];
+  boot.kernelModules = [ "kvm-intel" ];
   boot.extraModulePackages = [ ];
 
   fileSystems."/" =
-    { device = "/dev/disk/by-label/nixos";
-      fsType = "ext4";
+    { 
+	device = "/dev/disk/by-uuid/dca6ec1f-fe48-4484-abc5-feee60c01ce4";
+        fsType = "ext4";
     };
 
-  swapDevices = [ ];
+  fileSystems."/boot" =
+    { 
+	device = "/dev/disk/by-uuid/C3FB-7248";
+        fsType = "vfat";
+    };
 
-  virtualisation.virtualbox.guest.enable = true;
+  swapDevices =
+    [ 
+	{ 
+	    device = "/dev/disk/by-uuid/1348785d-6da6-45b0-89bc-1aff676f7851"; 
+	}
+    ];
+
+  powerManagement.cpuFreqGovernor = lib.mkDefault "powersave";
+  #hardware.cpu.intel.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
+  
+  hardware = {
+	#cpu.intel.updateMicrocode = true;
+        cpu.intel.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
+
+	enableAllFirmware = true;
+	opengl = {
+		enable = true;
+		extraPackages = with pkgs; [vaapiIntel libvdpau-va-gl vaapiVdpau ];
+	};
+	bluetooth.enable = true;
+#	pulseaudio.enable = true;
+  };
+
 }
