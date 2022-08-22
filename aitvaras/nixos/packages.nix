abb6a422b7262bfc2e6c7a6dfad2c856e504c3cc
CompEng001 <sb1501@canterbury.ac.uk>
2022-08-22 18:45:41 +0100

    mod: r packages

--- aitvaras/nixos/packages.nix
+++ aitvaras/nixos/packages.nix
@@ -10,7 +10,12 @@
     mpv = { youtubeSupport = true; };
   };
 
+   nixpkgs.config.permittedInsecurePackages = [
+                "python3.9-mistune-0.8.4"
+              ];
+   
   environment.systemPackages = with pkgs; [
+
 
     #[EDITORS]
     vim
@@ -29,6 +34,7 @@
     virt-viewer
     virtmanager
     #[PHD]
+		libreoffice
     jupyter
     mysql80
     zathura
@@ -36,11 +42,30 @@
     zotero
     #[PROGRAMMING]
     cargo
-    rustc
+    gcc
     lua
     powershell
     python3
-    gcc
+		(let
+  	  RStudio-with-my-packages = rstudioWrapper.override
+  	  { packages = with rPackages; 
+  	  	[ 
+  	  		janitor    # https://www.rdocumentation.org/packages/janitor/versions/2.1.0
+  	  		knitr      # https://www.rdocumentation.org/packages/knitr/versions/1.39
+					lubridate
+  	  		mlr        # https://www.rdocumentation.org/packages/mlr/versions/2.19.0
+  	  		mlr3       # https://www.rdocumentation.org/packages/mlr3/versions/0.14.0
+					rjson      # https://www.rdocumentation.org/packages/rjson/versions/0.2.21
+          tidyjson   # https://www.rdocumentation.org/packages/tidyjson/versions/0.3.1
+  	  		tidyverse  # https://www.rdocumentation.org/packages/tidyverse/versions/1.3.2
+  	  		tidymodels # https://www.rdocumentation.org/packages/tidymodels/versions/1.0.0
+  	  		xmlconvert # https://www.rdocumentation.org/packages/xmlconvert/versions/0.1.2
+  	  		xtable     # https://www.rdocumentation.org/packages/xtable/versions/1.8-4
+  	  	];
+  	  };
+	  in
+    RStudio-with-my-packages)    
+    rustc
     #[SOCIAL]
     discord
     signal-desktop
@@ -83,6 +108,7 @@
     spotify
     stow
     usbutils
+		volumeicon
     wget
     xclip
     zathura
