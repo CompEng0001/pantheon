b45fbd76d0970ec5bd9f960abfcf1420132177e3
CompEng0001 <sb1501@canterbury.ac.uk>
2022-08-24 07:58:55 +0100

    mod: updated == aitvaras

--- vakare/nixos/packages.nix
+++ vakare/nixos/packages.nix
@@ -3,7 +3,19 @@
 # and in the NixOS manual (accessible by running ‘nixos-help’).
 
 { pkgs, ... }: {
+  nixpkgs.config = {
+
+    virtualbox = { host.enableExtensionPack = true; };
+
+    mpv = { youtubeSupport = true; };
+  };
+
+   nixpkgs.config.permittedInsecurePackages = [
+                "python3.9-mistune-0.8.4"
+              ];
+   
   environment.systemPackages = with pkgs; [
+
 
     #[EDITORS]
     vim
@@ -19,30 +31,41 @@
     spotify-tui
     flavours
     teamviewer
+    virt-viewer
+    virtmanager
     #[PHD]
+		libreoffice
     jupyter
-    zettlr
-    zathura
-    zotero
     mysql80
+    zathura
+    zettlr
+    zotero
     #[PROGRAMMING]
     cargo
-    rustc
+    gcc
     lua
     powershell
-    python39
-    (let
-      my-python-packages = python-packages:
-        with python-packages; [
-          pandas
-          requests
-          numpy
-          markdown
-          matplotlib
-        ];
-      python-with-my-packages = python3.withPackages my-python-packages;
-    in python-with-my-packages)
-    gcc
+    python3
+		(let
+  	  RStudio-with-my-packages = rstudioWrapper.override
+  	  { packages = with rPackages; 
+  	  	[ 
+  	  		janitor    # https://www.rdocumentation.org/packages/janitor/versions/2.1.0
+  	  		knitr      # https://www.rdocumentation.org/packages/knitr/versions/1.39
+					lubridate
+  	  		mlr        # https://www.rdocumentation.org/packages/mlr/versions/2.19.0
+  	  		mlr3       # https://www.rdocumentation.org/packages/mlr3/versions/0.14.0
+					rjson      # https://www.rdocumentation.org/packages/rjson/versions/0.2.21
+          tidyjson   # https://www.rdocumentation.org/packages/tidyjson/versions/0.3.1
+  	  		tidyverse  # https://www.rdocumentation.org/packages/tidyverse/versions/1.3.2
+  	  		tidymodels # https://www.rdocumentation.org/packages/tidymodels/versions/1.0.0
+  	  		xmlconvert # https://www.rdocumentation.org/packages/xmlconvert/versions/0.1.2
+  	  		xtable     # https://www.rdocumentation.org/packages/xtable/versions/1.8-4
+  	  	];
+  	  };
+	  in
+    RStudio-with-my-packages)    
+    rustc
     #[SOCIAL]
     discord
     signal-desktop
@@ -53,25 +76,28 @@
     #[TOOLS]
     alsa-utils
     apparix
+    bat
     bc
     brightnessctl
     calc
+    check-uptime
+    cryptsetup
     curl
     dos2unix
     feh
     flameshot
-    fwupd
     git
+    guvcview
     htop
     iotop
     imagemagick
-    iwd
     lsd
     multilockscreen
+    mpv
     neofetch
+    pavucontrol
     peek
     psmisc
-    powertop
     pulseaudioFull
     qt5.qtquickcontrols
     qt5.qtquickcontrols2
@@ -81,6 +107,8 @@
     scrot
     spotify
     stow
+    usbutils
+		volumeicon
     wget
     xclip
     zathura
