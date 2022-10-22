523a27b9ce55034cdb155cc77dc2c77d70fceb8c
CompEng0001 <sb1501@canterbury.ac.uk>
2022-10-22 21:23:22 +0100

    mod: adapter for new setup

--- aitvaras/scripts/bash/lockscreen.sh
+++ aitvaras/scripts/bash/lockscreen.sh
@@ -1,12 +1,16 @@
 #! /usr/bin/env bash
 
 # File and path
-WALLPAPERPATH="$HOME/Git/personal/pantheon/aitvaras/Wallpaper/"
-WALLPAPERD0=".lockscreenEDP1.png"
+WALLPAPERPATH="$HOME/.config/Wallpaper/"
+WALLPAPERD0=".lockscreenDP1.png"
+WALLPAPERD1=".lockscreenHDMI1.png"
 
-scrot -D :0 "${WALLPAPERPATH}${WALLPAPERD0}"
+scrot -a $(xrandr | grep '\Wconnected\W' | grep -Po "\d+x\d+\+\d+\+\d+" | sed 's/x/+/' | awk -F "+" '{print $3","$4","$1","$2}' | head -n 1 | tail -n 1) "${WALLPAPERPATH}${WALLPAPERD0}"
 
-multilockscreen -u "${WALLPAPERPATH}${WALLPAPERD0}" --fx blur
+scrot -a $(xrandr | grep '\Wconnected\W' | grep -Po "\d+x\d+\+\d+\+\d+" | sed 's/x/+/' | awk -F "+" '{print $3","$4","$1","$2}' | head -n 2 | tail -n 1) "${WALLPAPERPATH}${WALLPAPERD1}"
+
+multilockscreen -u "${WALLPAPERPATH}${WALLPAPERD0}" -u ${WALLPAPERPATH}${WALLPAPERD1} --fx blur --display 1 2
+
 multilockscreen -l blur
 
-rm "${WALLPAPERPATH}${WALLPAPERD0}"
+rm "${WALLPAPERPATH}${WALLPAPERD0}" "${WALLPAPERPATH}${WALLPAPERD1}"
