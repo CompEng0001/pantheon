2c938e16145e140eae0f3788cd23275daa98146f
CompEng0001 <sb1501@canterbury.ac.uk>
2023-11-15 15:37:15 +0000

    del: removed obsolete files

--- aitvaras/scripts/bash/hibernate.sh
+++
@@ -1,7 +0,0 @@
-#! /usr/bin/env bash
-
-~/.config/scripts/bash/lockscreen.sh &
-
-sleep 3
-
-systemctl suspend
--- aitvaras/scripts/bash/phd.sh
+++
@@ -1,6 +0,0 @@
-#! /usr/bin/env bash
-
-i3-msg 'exec xdg-open https://www.overleaf.com/project' &
-i3-msg 'exec zotero' &
-i3-msg 'exec zathura' &
-i3-msg 'exec zettlr'&
