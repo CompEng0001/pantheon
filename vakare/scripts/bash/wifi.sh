af22d32414c3a744bc2d2aa209d3a4815a609dd2
CompEng0001 <sb1501@canterbury.ac.uk>
2024-01-16 07:37:19 +0000

    mod: new netwros added and changed nano to vim

--- vakare/scripts/bash/wifi.sh
+++ vakare/scripts/bash/wifi.sh
@@ -2,14 +2,13 @@
 
 IWDDIR="/var/lib/iwd/"
 declare -A networks
-networks=(["-e"]=eduroam.8021x ["-s"]=Southeastern_WiFi.open ["-p"]=CompEng0001.psk ["-N"]=TNCAP52FC59.psk
-	["-m"]=makerspace.psk ["-M"]=MACKNADE-FREE.open ["-n"]="NHS Wi-Fi.open" ["-f"]=HometelecomD2MZ5G.psk ["-r"]=BT-FNASJ9.psk ["-P"]="Pretty fly for a Wi Fi.psk")
+networks=(["-e"]=eduroam.8021x ["-s"]=Southeastern_WiFi.open ["-p"]=CompEng0001.psk ["-m"]=makerspace.psk ["-M"]=MACKNADE-FREE.open ["-n"]="NHS Wi-Fi.open" ["-f"]=HometelecomD2MZ5G.psk ["-r"]=BT-FNASJ9.psk ["-S"]="Pretty fly for a Wi Fi.psk")
 
 usage()
 {
    echo -e "wifi:\n\t-e eduroam [--gre || --cccu]"
    echo -e "\t-s Southeastern_WiFi\n\t-p CompEng0001\n\t-n NHS_Medway"
-   echo -e "\t-m makerspace\n\t-M Macknade\n\t-f flat\n\t-r parents\n\t-N Naomi's"
+   echo -e "\t-m makerspace\n\t-M Macknade\n\t-f flat\n\t-r parents\n\t-S Stephanie's"
    echo -e "\t--print [-network --print]\n\t--edit [-network --edit]"
    echo -e "\t-R restart iwd.service"
    echo -e "\tNOTE   *check /var/lib/iwd/ or use iwctl*"
@@ -18,14 +17,14 @@ usage()
 connection(){
 	iwctl station wlan0 scan
 	if   [[ $1 == -e ]];then
-		iwctl station wlan0 connect $(echo ${networks[-e]} | awk -F '.' '{print$NR}')
+		iwctl station wlan0 connect "$(echo ${networks[-e]} | awk -F '.' '{print$NR}')"
 	elif [[ $1 == -s ]];then
 		iwctl station wlan0 connect $(echo ${networks[-s]} | awk -F '.' '{print$NR}')
 		xdg-open https://southeastern.on.icomera.com
+	elif [[ $1 == -S ]];then
+		iwctl station wlan0 connect "$(echo ${networks[-S]} | awk -F '.' '{print$NR}')"
 	elif [[ $1 == -p ]];then
 		iwctl station wlan0 connect $(echo ${networks[-p]} | awk -F '.' '{print$NR}')
-	elif [[ $1 == -P ]];then
-		iwctl station wlan0 connect "$(echo ${networks[-P]} | awk -F '.' '{print$NR}')"
 	elif [[ $1 == -m ]]; then
 		iwctl station wlan0 connect $(echo ${networks[-m]} | awk -F '.' '{print$NR}')
 	elif [[ $1 == -M ]]; then
@@ -34,8 +33,6 @@ connection(){
 		iwctl station wlan0 connect "$(echo ${networks[-n]} | awk -F '.' '{print$NR}')"
 	elif [[ $1 == -f ]]; then
 		iwctl station wlan0 connect $(echo ${networks[-f]} | awk -F '.' '{print$NR}')
-	elif [[ $1 == -N ]]; then
-		iwctl station wlan0 connect $(echo ${networks[-N]} | awk -F '.' '{print$NR}')
 	elif [[ $1 == -r ]]; then
 		iwctl station wlan0 connect $(echo ${networks[-r]} | awk -F '.' '{print$NR}')
 	elif [[ $1 == -R ]]; then
@@ -44,11 +41,11 @@ connection(){
 }
 
 edit(){
-	sudo nano ${IWDDIR}${networks["$1"]}
+	sudo vim "${IWDDIR}${networks[$1]}"
 }
 
 print(){
-	sudo cat ${IWDDIR}${networks["$1"]}
+	sudo bat "${IWDDIR}${networks[$1]}" | grep -w Passphrase |  awk -F "=" '{print$2}'
 }
 
 
