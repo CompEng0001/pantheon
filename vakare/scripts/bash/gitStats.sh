8d72314888b993ab7887a6948825f8b69ce91dc7
CompEng0001 <sb1501@canterbury.ac.uk>
2024-01-23 20:24:23 +0000

    mod: now able to supply a branch

--- vakare/scripts/bash/gitStats.sh
+++ vakare/scripts/bash/gitStats.sh
@@ -1,7 +1,9 @@
 #! /usr/bin/env/ bash
 
 # This will print a summary table of authors, total commits, insertions, and deletions based on the provided git log output.
+# By default checks current branch or you can specify the branch you want.
 
+# Can see per branch all whole repo branch
 
 # Author         Commits   Insertions  Deletions   Insertion-Deletion
 # User1          6         97          11          86
@@ -10,7 +12,8 @@
 # User4          4         46          1           45
 # UserN          34        457         334         123
 
-git log --shortstat --pretty=format:'%an' | awk '
+git log $1 --shortstat --pretty=format:'%an' | awk '
+
   /^[a-zA-Z0-9_-]+$/ {
     author=tolower($1)
     author_width=length(author) > author_width ? length(author) : author_width
@@ -20,15 +23,15 @@ git log --shortstat --pretty=format:'%an' | awk '
     gsub(/,/, "", $4)
     gsub(/,/, "", $6)
     gsub(/,/, "", $8)
-    commits[author]++
-    insertions[author]+=$4
-    deletions[author]+=$6
+    c[author]++
+    i[author]+=$4
+    d[author]+=$6
   }
 
   END {
     printf "%-*s%-10s%-12s%-12s%-12s\n", author_width + 2, "Author", "Commits", "Insertions", "Deletions", "Insertion-Deletion"
-    for (author in commits) {
-      insertion_deletion_diff = insertions[author] - deletions[author]
-      printf "%-*s%-10d%-12d%-12d%-12d\n", author_width + 2, author, commits[author], insertions[author], deletions[author], insertion_deletion_diff
+    for (author in c) {
+      i_d_diff = i[author] - d[author]
+      printf "%-*s%-10d%-12d%-12d%-12d\n", author_width + 2, author, c[author], i[author], d[author], i_d_diff
     }
   }'
