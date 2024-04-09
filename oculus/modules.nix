8729a3db282e5c72dedeb2e3050a75537ef8775a
CompEng0001 <s.blair@gre.ac.uk>
2024-04-09 09:29:18 +0100

    add: import file

---
+++ oculus/modules.nix
@@ -0,0 +1,10 @@
+{ pkgs, ... }:
+
+{
+  imports =
+    [
+      /home/seb/Git/personal/pantheon/oculus/vim.nix
+      /home/seb/Git/personal/pantheon/oculus/packages.nix
+      /home/seb/Git/personal/pantheon/oculus/vm.nix
+    ];
+}
